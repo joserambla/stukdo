b2edddac21976be95072b1058fbfb043ad9a5bb5
Jose <jose.rambla@outlook.com>
2015-04-28 18:47:00 +0200

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  
+  get 'about' => 'pages#about'
+  get 'exxxpat' => 'pages#expat'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
